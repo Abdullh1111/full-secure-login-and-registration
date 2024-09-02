eebd4792d6c66881b2abc3ae702296d6ecb82af1
Abdullh1111 <abdullah4474032@gmail.com>
2024-09-02 07:08:45 +0600

    add error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,19 @@
 import express from 'express'
+import globalErrorHandler from './ErrorHandler/glbalErrorHandler'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 const app = express()
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({origin: "http://localhost:5173", credentials: true}))
+app.use(cookieParser())
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(globalErrorHandler)
 export default app
