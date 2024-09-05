93ef9337363b14a71744e0a056690c7e68ea3503
Abdullh1111 <138347870+Abdullh1111@users.noreply.github.com>
2024-09-05 23:41:19 +0600

    Update user.controller.ts

--- backend/src/module/users/user.controller.ts
+++ backend/src/module/users/user.controller.ts
@@ -12,7 +12,7 @@ const login = catchAsync(async (req, res) => {
     res.status(201).cookie('token',token,{
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        secure: true, 
+        secure: false, 
         sameSite: "none"
     }).json({
         success: true,
