05f673de875ce018acb0153ded2790b99bbb333e
Abdullh1111 <138347870+Abdullh1111@users.noreply.github.com>
2024-09-05 23:12:48 +0600

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({origin: "http://localhost:5173", credentials: true}))
+app.use(cors({origin: "https://full-secure-login-and-registration-1.onrender.com", credentials: true}))
 app.use(cookieParser())
 
 // routes
